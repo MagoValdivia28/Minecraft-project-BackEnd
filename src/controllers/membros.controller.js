9121120c578cf94f0e8e707da99500fd1e2ef934
Pedro <pedrosouza.senai564@gmail.com>
2023-12-04 16:24:16 -0300

    Add member verification and controller

---
+++ src/controllers/membros.controller.js
@@ -0,0 +1,3 @@
+import { Membro } from '../models/membro.model.js';
+import { ListaMembros } from '../models/listaMembros.model.js';
+import { verificacoesMembro } from '../components/verifications.js';
