0b952e32ca8a2f00a4277673e925eefaff001704
ElMatheus <matheus.goncalves5@aluno.senai.br>
2023-12-07 10:03:44 -0300

    fit: all filtered

--- src/components/Verifications.js
+++ src/components/Verifications.js
@@ -17,7 +17,7 @@ export const verificacoesEquipamento = (nome, descricao, material, tipo, dano, d
     if (!cor) {
         errors.push("Cor não informada");
     }
-    if (tipo !== "espada" && tipo !== "capacete" && tipo !== "peitoral" && tipo !== "calça" && tipo !== "bota") {
+    if (tipo !== "espada" && tipo !== "capacete" && tipo !== "peitoral" && tipo !== "calca" && tipo !== "bota") {
         errors.push("Tipo deve ser espada, capacete, peitoral, calça ou bota");
     }
     if (isNaN(dano) || isNaN(defesa)) {
